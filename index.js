bac84cedd2029664eca46712a9495bb02f78cb0c
Noelle <noelle.maingi@student.moringaschool.com>
2023-06-15 23:43:11 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+employee = {
+  name: "Sam",
+  streetAddress: "11 Broadway",
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const newEmployee = { ...employee };
+  newEmployee[key] = value;
+  return newEmployee;
+}
+
+let employee;
+
+beforeEach(function () {
+  employee = [];
+  employee.name = "Sam";
+});
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+
+  return employee;
+}
+ function deleteFromEmployeeByKey(employee, key){
+    const { [key]: deleted, ...newEmployee} = employee;
+    return newEmployee;
+ }
+
+ function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+ }
